7146430027aa82c90a13d1e17e841e5cb0c1e96f
rikidoufox <93853732+rikidoufox@users.noreply.github.com>
2024-06-11 14:17:00 +0530

    Update reversi.c

--- reversi.c
+++ reversi.c
@@ -1,23 +1,11 @@
-//
-// Author:
-//
-#include "reversi.h"
-#define MAX_BOARD_SIZE 26
+#include "lab8part2.h"
+#include "liblab8part2.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
 
-void printBoard(char board[][MAX_BOARD_SIZE], int n) {
-    printf("  ");
-    for (int i = 0; i < n; i++) {
-        printf("%c", 'a' + i);
-    }
-    printf("\n");
-    for (int i = 0; i < n; i++) {
-        printf("%c ", 'a' + i);
-        for (int j = 0; j < n; j++) {
-            printf("%c", board[i][j]);
-        }
-        printf("\n");
-    }
-}
+#define MAX_BOARD_SIZE 26
 
 bool positionInBounds(int n, int row, int col) {
     return row >= 0 && row < n && col >= 0 && col < n;
@@ -72,7 +60,115 @@ bool isValidMove(char board[][MAX_BOARD_SIZE], int n, int row, int col, char col
     return false;
 }
 
-void makeMove(char board[][MAX_BOARD_SIZE], int n, int row, int col, char colour) {
+int minimax(char board[][MAX_BOARD_SIZE], int n, char turn, int depth, bool maximizingPlayer);
+
+int makeMove(const char board[][MAX_BOARD_SIZE], int n, char turn, int *row, int *col) {
+    int bestScore = -1000;
+    *row = -1;
+    *col = -1;
+
+    char newBoard[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i][j] == 'U' && isValidMove(board, n, i, j, turn)) {
+                for (int x = 0; x < n; x++) {
+                    for (int y = 0; y < n; y++) {
+                        newBoard[x][y] = board[x][y];
+                    }
+                }
+                newBoard[i][j] = turn;
+
+                int score = minimax(newBoard, n, (turn == 'W') ? 'B' : 'W', 4, false);
+                if (score > bestScore) {
+                    bestScore = score;
+                    *row = i;
+                    *col = j;
+                }
+            }
+        }
+    }
+
+    return (bestScore != -1000) ? 1 : 0; // Indicate success if a move is found
+}
+int minimax(char board[][MAX_BOARD_SIZE], int n, char turn, int depth, bool maximizingPlayer) {
+    if (depth == 0) {
+        return evaluateBoard(board, n);
+    }
+
+    if (maximizingPlayer) {
+        int bestScore = -1000;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'U' && isValidMove(board, n, i, j, turn)) {
+                    char newBoard[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+                    for (int x = 0; x < n; x++) {
+                        for (int y = 0; y < n; y++) {
+                            newBoard[x][y] = board[x][y];
+                        }
+                    }
+                    newBoard[i][j] = turn;
+
+                    int score = minimax(newBoard, n, (turn == 'W') ? 'B' : 'W', depth - 1, false);
+                    if (score > bestScore) {
+                        bestScore = score;
+                    }
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        int bestScore = 1000;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'U' && isValidMove(board, n, i, j, turn)) {
+                    char newBoard[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+                    for (int x = 0; x < n; x++) {
+                        for (int y = 0; y < n; y++) {
+                            newBoard[x][y] = board[x][y];
+                        }
+                    }
+                    newBoard[i][j] = turn;
+
+                    int score = minimax(newBoard, n, (turn == 'W') ? 'B' : 'W', depth - 1, true);
+                    if (score < bestScore) {
+                        bestScore = score;
+                    }
+                }
+            }
+        }
+        return bestScore;
+    }
+}
+int evaluateBoard(char board[][MAX_BOARD_SIZE], int n) {
+    int score = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i][j] == 'W') {
+                score += 1;
+            } else if (board[i][j] == 'B') {
+                score -= 1;
+            }
+        }
+    }
+    return score;
+}
+void printBoard(char board[][MAX_BOARD_SIZE], int n) {
+    printf("  ");
+    for (int i = 0; i < n; i++) {
+        printf("%c", 'a' + i);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++) {
+        printf("%c ", 'a' + i);
+        for (int j = 0; j < n; j++) {
+            printf("%c", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void updateBoard(char board[][MAX_BOARD_SIZE], int n, int row, int col, char colour) {
     board[row][col] = colour;
 
     for (int dRow = -1; dRow <= 1; dRow++) {
@@ -96,62 +192,18 @@ void makeMove(char board[][MAX_BOARD_SIZE], int n, int row, int col, char colour
 }
 
 void computerMove(char board[][MAX_BOARD_SIZE], int n, char playerColour) {
-    char computerColour = (playerColour == 'W') ? 'B' : 'W'; // Determine computer's color
+    char computerColour = (playerColour == 'W') ? 'B' : 'W';
 
-    int bestRow = -1;
-    int bestCol = -1;
-    int maxFlips = -1;
-    int blankCount = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i][j] == 'U') {
-                blankCount++;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i][j] == 'U' && isValidMove(board, n, i, j, computerColour)) {
-                int flips = 0;
+    int bestRow, bestCol;
+    int moveStatus = makeMove((const char (*)[MAX_BOARD_SIZE])board, n, computerColour, &bestRow, &bestCol);
 
-                for (int dRow = -1; dRow <= 1; dRow++) {
-                    for (int dCol = -1; dCol <= 1; dCol++) {
-                        if (dRow == 0 && dCol == 0) {
-                            continue;
-                        }
-
-                        if (checkLegalInDirection(board, n, i, j, computerColour, dRow, dCol)) {
-                            int newRow = i + dRow;
-                            int newCol = j + dCol;
-
-                            while (board[newRow][newCol] != computerColour) {
-                                flips++;
-                                newRow += dRow;
-                                newCol += dCol;
-                            }
-                        }
-                    }
-                }
-
-                if (flips > maxFlips) {
-                    maxFlips = flips;
-                    bestRow = i;
-                    bestCol = j;
-                }
-            }
-        }
-    }
-
-    if (bestRow != -1 && bestCol != -1) {
-        makeMove(board, n, bestRow, bestCol, computerColour);
+    if (moveStatus == 1) {
+        updateBoard(board, n, bestRow, bestCol, computerColour);
         printf("Computer places %c at %c%c.\n", computerColour, 'a' + bestRow, 'a' + bestCol);
         printBoard(board, n);
+    } else {
+        printf("%c player has no valid move.\n", computerColour);
     }
-    else{
-        if(blankCount!=0){
-            printf("%c player has no valid move.\n", computerColour);
-        }
-   }
 }
 
 int main(void) {
@@ -171,7 +223,7 @@ int main(void) {
     printf("Computer plays (B/W): ");
     scanf(" %c", &playerColour);
 
-    char computerColour = (playerColour == 'W') ? 'B' : 'W'; // Determine computer's color
+    char computerColour = (playerColour == 'W') ? 'B' : 'W';
 
     board[n / 2 - 1][n / 2 - 1] = 'W';
     board[n / 2][n / 2] = 'W';
@@ -182,7 +234,7 @@ int main(void) {
     if (playerColour == 'B') {
         computerMove(board, n, computerColour);
     }
-    int check=0;
+
     while (true) {
         printf("Enter move for colour %c (RowCol): ", computerColour);
         scanf(" %c%c", &row, &col);
@@ -195,7 +247,7 @@ int main(void) {
             printf("%c player wins.\n", playerColour);
             return 0;
         } else {
-            makeMove(board, n, rowIndex, colIndex, computerColour);
+            updateBoard(board, n, rowIndex, colIndex, computerColour);
             printBoard(board, n);
             computerMove(board, n, computerColour);
         }
@@ -251,4 +303,3 @@ int main(void) {
 
     return 0;
 }
-
